a4dba4c94e193062b115ec2f4cdb3b6077de6b10
diobayu22 <diobayupranata22@gmail.com>
2024-06-04 03:48:34 +0700

    update : navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,23 @@
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import { useEffect, useState } from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
 
-export default function Layout({children}){
-    return(
-        <>
-            <Navbar />
-            {children}
-            <Footer />
-        </>
-    )
+export default function Layout({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  useEffect(() => {
+    // Periksa apakah ada refresh token di local storage
+    const refreshToken = localStorage.getItem('refresh_token')
+    if (refreshToken) {
+      // Jika ada, set isAuthenticated menjadi true
+      setIsAuthenticated(true)
+    }
+  }, []) // Kode ini hanya dijalankan sekali setelah komponen dipasang
+  return (
+    <>
+      <Navbar isAuthenticated={isAuthenticated} />
+      {children}
+      <Footer />
+    </>
+  )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,70 @@
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
-export default function Navbar() {
-    return (
-        <nav className="navbar sticky-top navbar-expand-lg navbar-dark main-nav">
-            <div className="container">
-                <Link className="navbar-brand" to="/">
-                    <img src="/images/global/logo.png" alt="" />
+const Navbar = ({ isAuthenticated }) => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    // Perform logout logic here
+    localStorage.removeItem('refresh_token')
+    console.log('User logged out')
+    navigate('/login') // Redirect to login page after logout
+  }
+
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark main-nav">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <img src="/images/global/logo.png" alt="" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto py-2">
+            <li className="nav-item px-3">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item  px-3">
+              <Link className="nav-link" to="/garasi">
+                Garasi
+              </Link>
+            </li>
+            {isAuthenticated ? (
+              <li className="nav-item px-3">
+                <Link className="nav-link" to="/profile">
+                  Profile
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
+              </li>
+            ) : (
+              <li className="nav-item px-3">
+                <Link className="button1" to="/masuk">
+                  Login
+                </Link>
+              </li>
+            )}
+            {isAuthenticated && (
+              <li className="nav-item px-3">
+                <button className="button1" onClick={handleLogout}>
+                  Logout
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto py-2">
-                        <li className="nav-item px-3">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item  px-3">
-                            <Link className="nav-link" to="/garasi">Garasi</Link>
-                        </li>
-                        <Link className="button1" to='/masuk'>Login</Link>
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-    )
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
+
+export default Navbar
